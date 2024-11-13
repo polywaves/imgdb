df24e4a643332bc1e93856b5ea55f3d24e977b61
Daniel <poly.wave@yandex.ru>
2024-11-14 01:33:28 +0300

    init

--- app/providers/weaviate_provider.py
+++ app/providers/weaviate_provider.py
@@ -7,10 +7,10 @@ collection_name = 'test1'
 
 
 client = weaviate.connect_to_custom(
-  http_host="weaviate",
+  http_host="87.242.104.141",
   http_port=8080,
   http_secure=False,
-  grpc_host="weaviate",
+  grpc_host="87.242.104.141",
   grpc_port=50051,
   grpc_secure=False
 )
