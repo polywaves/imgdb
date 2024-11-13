290a6c8afcac1a137fc1f9723c39043535b5f1df
Daniel <poly.wave@yandex.ru>
2024-11-14 02:49:33 +0300

    update

--- training.py
+++ training.py
@@ -2,7 +2,8 @@ import requests
 import json
 import pandas
 
-url = "http://0.0.0.0:8000/api/v1/image/training/"
+# url = "http://0.0.0.0:8000/api/v1/image/training/"
+url = "http://87.242.104.141:8000/api/v1/vector/training/"
 headers = {
   'Content-Type': 'application/json'
 }
@@ -11,14 +12,14 @@ collection_name="test1"
 images = pandas.read_csv("./imgs_396k.csv", on_bad_lines="skip", sep=";", low_memory=False, names=["id", "uid", "img"])
 
 for i, row in images.iterrows():
-  if (i > 3732):
-    payload = json.dumps({
-      # "collection_name": collection_name,
+  if (i > 0):
+    params = {
       "url": row["img"],
       "uid": int(row["uid"])
-    })
-    response = requests.request("PUT", url, headers=headers, data=payload)
+    }
+    response = requests.request("GET", url, headers=headers, params=params)
     
+    print(params)
     print(f"TOTAL: {i}")
     print(response.text)
   else:
