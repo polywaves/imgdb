2a3c1bc6b252e35064d3807f88eb6b6d542dc33e
Daniel <poly.wave@yandex.ru>
2024-11-14 20:15:22 +0300

    update

--- app/api/v1/graf.py
+++ app/api/v1/graf.py
@@ -1,13 +1,31 @@
+from time import time
+from datetime import datetime
 from fastapi import APIRouter, HTTPException
+from app.mongo import db
 from app.utils.logger_util import logger
 
 router = APIRouter()
 
 
+posts_collection = db.get_collection("posts")
+
+
 @router.get("/uni_vectors", tags=["Get count of uniq vectors"])
 async def uni_vectors():
   try:
-    pass
+    result = 0
+
+    return {
+      "data": [
+        {
+          "rkey": time(),
+          "dt": datetime.now(),
+          "val1": result
+        }
+      ],
+      "result": 1, 
+      "speed": 0
+    }
   except Exception as e:
     logger.error(e)
     raise HTTPException(status_code=500, detail=str(e))
@@ -15,35 +33,67 @@ async def uni_vectors():
 
 @router.get("/uni_ids", tags=["Get count of uniq image ids"])
 async def uni_ids():
-  try:
-    pass
-  except Exception as e:
-    logger.error(e)
-    raise HTTPException(status_code=500, detail=str(e))
+  result = 0
+
+  return {
+    "data": [
+      {
+        "rkey": time(),
+        "dt": datetime.now(),
+        "val1": result
+      }
+    ],
+    "result": 1, 
+    "speed": 0
+  }
   
 
 @router.get("/uni_posts", tags=["Get count of uniq post ids"])
 async def uni_posts():
-  try:
-    pass
-  except Exception as e:
-    logger.error(e)
-    raise HTTPException(status_code=500, detail=str(e))
+  result = await posts_collection.count_documents({})
+
+  return {
+    "data": [
+      {
+        "rkey": time(),
+        "dt": datetime.now(),
+        "val1": result
+      }
+    ],
+    "result": 1, 
+    "speed": 0
+  }
   
 
 @router.get("/day_requests", tags=["Get count of last 24h requests"])
 async def day_requests():
-  try:
-    pass
-  except Exception as e:
-    logger.error(e)
-    raise HTTPException(status_code=500, detail=str(e))
+  result = 0
+
+  return {
+    "data": [
+      {
+        "rkey": time(),
+        "dt": datetime.now(),
+        "val1": result
+      }
+    ],
+    "result": 1, 
+    "speed": 0
+  }
 
 
 @router.get("/usage", tags=["Get current usage of cpu and gpu"])
-async def day_requests():
-  try:
-    pass
-  except Exception as e:
-    logger.error(e)
-    raise HTTPException(status_code=500, detail=str(e))
+async def usage():
+  result = 0
+
+  return {
+    "data": [
+      {
+        "rkey": time(),
+        "dt": datetime.now(),
+        "val1": result
+      }
+    ],
+    "result": 1, 
+    "speed": 0
+  }
