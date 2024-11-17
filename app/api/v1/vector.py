d6f9d1b01e6107990d42bcfa2bb6d92d59d330ae
Daniel <poly.wave@yandex.ru>
2024-11-18 00:09:05 +0300

    update

--- app/api/v1/vector.py
+++ app/api/v1/vector.py
@@ -1,4 +1,5 @@
-import json
+import time
+from urllib.parse import unquote
 from fastapi import APIRouter, HTTPException, UploadFile, File
 from app.providers import weaviate_provider
 from app.models import vector_model
@@ -57,7 +58,10 @@ async def delete_posts(post_ids: list):
       })
 
       for image in post["images"]:
-        weaviate_provider.delete_image_by_uid(uid=int(image["img_id"]))
+        try:
+          weaviate_provider.delete_image_by_uid(uid=int(image["img_id"]))
+        except Exception:
+          logger.debug(f"Vector uid {image.img_id} not found")
 
       await posts_collection.delete_one({
         "id": post_id
@@ -72,37 +76,48 @@ async def delete_posts(post_ids: list):
 
 @router.get("/delete_posts_by_ids", tags=["Delete post and image vectors by post ids throught ,"])
 async def delete_posts_by_ids(post_ids: str):
+  start_time = time.time()
+
   deleted = await delete_posts(post_ids=post_ids.split(','))
   
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "deleted": deleted
   }
   
 
 @router.post("/delete_posts_by_ids", tags=["Delete post and image vectors by post ids"])
 async def delete_posts_by_ids(params: vector_model.DeletePostsByIds):
+  start_time = time.time()
+
   deleted = await delete_posts(post_ids=params.post_ids)
   
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "deleted": deleted
   }
 
 
 @router.post("/search_by_url", tags=["Search near vectors by url"])
 async def search_by_url(url: str):
+  start_time = time.time()
+
   image = image_util.from_url_to_base64(url)
   posts = await search_posts(image=image)
 
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "posts": posts
   }
 
 
 @router.get("/search_by_img_id", tags=["Search near vectors by existing img id"])
 async def search_by_img_id(img_id: int):
+  start_time = time.time()
+
   post = await posts_collection.find_one({
     "images.img_id": int(img_id)
   })
@@ -110,6 +125,7 @@ async def search_by_img_id(img_id: int):
   if not post:
     return {
       "status": 1,
+      "speed": time.time() - start_time,
       "posts": []
     }
 
@@ -121,27 +137,36 @@ async def search_by_img_id(img_id: int):
 
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "posts": posts
   }
 
 
 @router.post("/search_by_upload", tags=["Search near vectors by uploading image"])
 async def search_by_upload(image: UploadFile = File()):
+  start_time = time.time()
+
   image = image_util.to_base64(await image.read())
   posts = await search_posts(image=image)
 
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "posts": posts
   }
   
 
 @router.post("/training", tags=["Training by json"])
 async def training_by_json(params: vector_model.TrainingByJson):
+  start_time = time.time()
+
   items = list()
   images = list()
   for image in params.images:
-    weaviate_provider.delete_image_by_uid(uid=int(image.img_id))
+    try:
+      weaviate_provider.delete_image_by_uid(uid=int(image.img_id))
+    except Exception:
+      logger.debug(f"Vector uid {image.img_id} not found")
 
     items.append({
       "image": image_util.from_url_to_base64(image.img),
@@ -157,12 +182,25 @@ async def training_by_json(params: vector_model.TrainingByJson):
   await posts_collection.delete_one({
     "id": params.id
   })
-  await posts_collection.insert_one(params.model_dump())
 
+  ## Decode texts
+  params.by = unquote(params.by)
+  params.item_name = unquote(params.item_name)
+  params.text = unquote(params.text)
+
+  options = list()
+  for option in params.options:
+    options.append(unquote(option))
+  params.options = options
+
+  post = params.model_dump()
+
+  await posts_collection.insert_one(post)
   weaviate_provider.create_image_vector(items=items)
 
   return {
     "status": 1,
+    "speed": time.time() - start_time,
     "post_id": params.id,
     "images": images
   }
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-from fastapi import FastAPI, Request
-# from fastapi.middleware.cors import CORSMiddleware
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.utils.logger_util import logger
 
 from app.api import v1
@@ -9,31 +9,15 @@ app = FastAPI(
   redirect_slashes=False
 )
 
-# app.add_middleware(
-#   CORSMiddleware,
-#   allow_origins=["*"],
-#   allow_credentials=True,
-#   allow_methods=["*"],
-#   allow_headers=["*"],
-# )
-
-# Whitelisted IPs
-# WHITELISTED_IPS = ["5.61.63.91", "46.138.188.236", "87.242.104.141", "0.0.0.0", "127.0.0.1", "192.168.65.1"]
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next) -> any:
-  start_time = time.time()
-  # Get client IP
-  ip = str(request.client.host)
-  logger.info(f"Client ip is {ip}")
-
-  # if ip not in WHITELISTED_IPS:
-  #   raise HTTPException(status_code=500, detail=f"IP {ip} is not allowed to access this resource.")  
-
-  response = await call_next(request)
-  logger.info(f"Response time is {time.time() - start_time} sec")
-
-  return response
+if os.environ["MODE"] == 'development':
+  logger.debug("Development mode")
+  app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+  )
 
 app.include_router(v1.router, prefix='/api/v1')
 
