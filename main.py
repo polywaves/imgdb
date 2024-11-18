46ad32e7f3bf2b334863af2d4861bd232cc2c143
Daniel <poly.wave@yandex.ru>
2024-11-18 17:34:47 +0300

    update

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if os.environ["MODE"] == 'development':
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next) -> any:
-  logger.debug(request.json())
+  logger.debug(await request.json())
 
   response = await call_next(request)
 
