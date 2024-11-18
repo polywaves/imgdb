b5f14c8f0b969cc99066619c1196fe4f7f601ffb
Daniel <poly.wave@yandex.ru>
2024-11-18 19:10:46 +0300

    update

--- pull.sh
+++ pull.sh
@@ -3,4 +3,4 @@
 git pull
 # docker -f docker-compose.prod.yml compose down
 docker compose -f docker-compose.prod.yml up -d --build
-docker compose -f docker-compose.prod.yml logs -f
+docker compose -f docker-compose.prod.yml logs -f api
