604345a0a09939ae14b751304240481cb8c12bb3
Daniel <poly.wave@yandex.ru>
2024-11-18 15:45:19 +0300

    update

--- app/utils/response_util.py
+++ app/utils/response_util.py
@@ -3,8 +3,8 @@ from app.utils.logger_util import logger
 
 
 def response(data: dict, start_time) -> dict:
-  speed = int(time.time() - start_time)
-  data["speed"] = speed * 1000
+  speed = time.time() - start_time
+  data["speed"] = int(speed * 1000)
 
   logger.debug(f"RESPONSE TIME: {speed}")
 
