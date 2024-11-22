57d335fc6379b046b52e4c0e5c53767df8523aa5
Daniel <poly.wave@yandex.ru>
2024-11-22 19:37:52 +0300

    update

--- app/mongo.py
+++ app/mongo.py
@@ -12,11 +12,11 @@ vector_hashes_collection = db.get_collection("vector_hashes")
 
 ## Make migrations
 async def migrate():
-  posts_indexes = [
-    IndexModel([("id", DESCENDING)]),
-    IndexModel([("images.img_id", DESCENDING)])
-  ]
+  await posts_collection.create_index("id")
+  await posts_collection.create_index("images.img_id")
 
-  await posts_collection.create_indexes(posts_indexes)
   await post_image_ids_collection.create_index("id", unique=True)
+  await post_image_ids_collection.create_index("post_id")
+
   await vector_hashes_collection.create_index("hash", unique=True)
+  await vector_hashes_collection.create_index("post_id")
