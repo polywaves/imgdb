8788c32dc532ec1f1b022c20bf3d32e587aae2f4
Daniel <poly.wave@yandex.ru>
2024-12-02 08:28:10 +0300

    update

--- push.sh
+++ push.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-docker build -t polywaves/api .
-docker push polywaves/api
-
-# docker build -t polywaves/i2v ./i2v
-# docker push polywaves/i2v
-
 git add .
 git commit -m update
 git push
