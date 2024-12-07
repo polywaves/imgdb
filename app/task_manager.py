e2a06c88eb17172c2df9c8b77d2279353362c896
Daniel <poly.wave@yandex.ru>
2024-12-07 23:23:54 +0300

    update

--- app/task_manager.py
+++ app/task_manager.py
@@ -1,4 +1,4 @@
-from app.mongo import posts_collection
+from app.mongo import posts_collection, post_image_ids_collection, vector_hashes_collection
 from app.utils.logger_util import logger
 from app.utils.text_util import urldecode
 from bson.objectid import ObjectId
@@ -33,20 +33,22 @@ async def run():
 
 
 async def fix():
-  posts = await posts_collection.find().to_list()
+  rows = await post_image_ids_collection.find({
+    "created_at": None
+  }).to_list()
 
-  for post in posts:
-    timestamp = post["_id"].generation_time.timestamp()
-    post_id = post["id"]
+  for row in rows:
+    timestamp = row["_id"].generation_time.timestamp()
+    id = row["id"]
 
-    await posts_collection.update_one({
-      "_id": post["_id"]
+    await post_image_ids_collection.update_one({
+      "_id": row["_id"]
     }, {
       "$set": {
         "created_at": timestamp
       }
     })
 
-    logger.info(f"timestamp {timestamp} for {post_id} updated")
+    logger.info(f"timestamp {timestamp} for {id} updated")
 
 
