320a4beb410df956737f05e734d03949143b1102
Daniel <poly.wave@yandex.ru>
2024-12-07 22:17:03 +0300

    update

--- app/task_manager.py
+++ app/task_manager.py
@@ -2,6 +2,7 @@ from app.mongo import posts_collection
 from app.utils.logger_util import logger
 from app.utils.text_util import urldecode
 from bson.objectid import ObjectId
+from datetime import datetime, timedelta
 
 ## Make migrations
 async def run():
@@ -35,6 +36,14 @@ async def fix():
   posts = await posts_collection.find().limit(100).to_list()
 
   for post in posts:
-    logger.info(post["_id"].generation_time)
+    # await posts_collection.update_one({
+    #   "_id": post["_id"]
+    # }, {
+    #   "$set": {
+    #     "sizes": sizes
+    #   }
+    # })
+
+    logger.info(datetime(post["_id"].generation_time))
 
 
