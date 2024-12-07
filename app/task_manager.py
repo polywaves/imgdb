d120eed9e8e114b41c18648a08967971720e9ff8
Daniel <poly.wave@yandex.ru>
2024-12-07 22:13:23 +0300

    update

--- app/task_manager.py
+++ app/task_manager.py
@@ -1,29 +1,40 @@
 from app.mongo import posts_collection
 from app.utils.logger_util import logger
 from app.utils.text_util import urldecode
+from bson.objectid import ObjectId
 
 ## Make migrations
 async def run():
-  # Rewinding posts
-  posts = await posts_collection.find({
-    "sizes": {
-      "$regex": "%"
-    }
-  }).to_list()
+  await fix()
+
+  # # Rewinding posts
+  # posts = await posts_collection.find({
+  #   "sizes": {
+  #     "$regex": "%"
+  #   }
+  # }).to_list()
+
+  # for post in posts:
+  #   sizes = list()
+  #   for size in post["sizes"]:
+  #     sizes.append(urldecode(size))
+
+  #   await posts_collection.update_one({
+  #     "_id": post["_id"]
+  #   }, {
+  #     "$set": {
+  #       "sizes": sizes
+  #     }
+  #   })
+
+  #   logger.info(sizes)
+
+
+
+async def fix():
+  posts = await posts_collection.find().limit(100).to_list()
 
   for post in posts:
-    sizes = list()
-    for size in post["sizes"]:
-      sizes.append(urldecode(size))
-
-    await posts_collection.update_one({
-      "_id": post["_id"]
-    }, {
-      "$set": {
-        "sizes": sizes
-      }
-    })
-
-    logger.info(sizes)
+    logger.info(post["_id"].generation_time)
 
 
