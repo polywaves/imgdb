c8554661c8f11c6bfdffd60fe0e29545ef6e9322
Daniel <poly.wave@yandex.ru>
2024-12-10 03:02:10 +0300

    update

--- app/task_manager.py
+++ app/task_manager.py
@@ -6,55 +6,32 @@ from datetime import datetime, timedelta
 
 ## Make migrations
 async def run():
-  await fix(posts_collection)
-  await fix(post_image_ids_collection)
-  await fix(vector_hashes_collection, id_key="img_id")
-
-  # # Rewinding posts
-  # posts = await posts_collection.find({
-  #   "sizes": {
-  #     "$regex": "%"
-  #   }
-  # }).to_list()
-
-  # for post in posts:
-  #   sizes = list()
-  #   for size in post["sizes"]:
-  #     sizes.append(urldecode(size))
-
-  #   await posts_collection.update_one({
-  #     "_id": post["_id"]
-  #   }, {
-  #     "$set": {
-  #       "sizes": sizes
-  #     }
-  #   })
-
-  #   logger.info(sizes)
+  await fix(posts_collection, {
+    "id": 1,
+    "posted": 1
+  })
 
 
 
-async def fix(collection, id_key: str = "id"):
+async def fix(collection, id_key: dict):
   rows = await collection.find({
-    "created_at": {
+    "creation_date": {
       "$exists": False
     }
-  }, {
-    id_key: 1
-  }).to_list()
+  }, repr).limit(10).to_list()
 
   for row in rows:
-    timestamp = row["_id"].generation_time.timestamp()
-    id = row[id_key]
+    posted = row["posted"]
+    creation_date = datetime.strptime(f"{posted}.{datetime.now().strftime('%y')}", "%d.%m.%y")
 
     await collection.update_one({
       "_id": row["_id"]
     }, {
       "$set": {
-        "created_at": timestamp
+        "creation_date": creation_date
       }
     })
 
-    logger.info(f"timestamp {timestamp} for {id} updated")
+    logger.info(f"creation date {creation_date} for {id} updated")
 
 
