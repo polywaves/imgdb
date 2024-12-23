7f07e984417ae7bbc19fd4ffd6cc5598113b726d
Daniel <poly.wave@yandex.ru>
2024-12-23 18:21:53 +0300

    update

--- app/api/v1/cmd.py
+++ app/api/v1/cmd.py
@@ -1,10 +1,13 @@
-import subprocess
 from time import time
 from fastapi import APIRouter
+from python_on_whales import DockerClient
 from app.utils.logger_util import logger
 from app.utils import response_util
 
 router = APIRouter()
+
+
+docker = DockerClient(compose_files=["./docker-compose.prod.yml"])
 
 
 @router.get("/restart_neuro", tags=["Restart neuro nodes"])
@@ -17,12 +20,10 @@ async def restart_neuro():
   }
   nodes = 6
   for node in range(nodes):
-    cmd = f"docker compose -f docker-compose.prod.yml down node{node}"
-    data["down"].append(subprocess.call(cmd, shell=True))
+    data["down"].append(docker.compose.down(f"node{node}"))
 
   for node in range(nodes):
-    cmd = f"docker compose -f docker-compose.prod.yml up -d node{node}"
-    data["up"].append(subprocess.call(cmd, shell=True))
+    data["up"].append(docker.compose.up(f"node{node}"))
 
   logger.debug(data)
 
