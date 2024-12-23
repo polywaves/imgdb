0ea528a3d4491f1fe3affeed22f8fafb47804ecb
Daniel <poly.wave@yandex.ru>
2024-12-23 18:07:06 +0300

    update

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1 import vector, graf
+from app.api.v1 import vector, graf, cmd
 
 
 
@@ -8,4 +8,4 @@ router = APIRouter()
 
 router.include_router(vector.router, prefix='/vector')
 router.include_router(graf.router, prefix='/graf')
-router.include_router(graf.router, prefix='/cmd')
+router.include_router(cmd.router, prefix='/cmd')
