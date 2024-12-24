93771b79b2085ba52da19f720fb2f9d1cb4ae0c3
Daniel <poly.wave@yandex.ru>
2024-12-24 09:02:18 +0300

    update

--- app/api/v1/cmd.py
+++ app/api/v1/cmd.py
@@ -38,8 +38,9 @@ def get_portainer_containers(jwt: str, filter: str) -> str:
   containers = requests.request("GET", os.path.join(os.environ["PORTAINER_HOST"], "endpoints/1/docker/v1.41/containers/json"), headers=headers, params=payload).json()
   data = list()
   for container in containers:
-    if filter in container["Names"][0]:
-      data.append(container)      
+    logger.debug(container)
+    # if filter in container["Names"][0]:
+    #   data.append(container)      
 
   return data
 
